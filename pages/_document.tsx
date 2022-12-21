829d5ba62198dd28c5b61f1a7bc581ebedb1b35d
Charles Yang <yarlescy@gmail.com>
2022-12-21 17:53:26 -0600

    added navbar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import IYarlesNavbar from './_navbar'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <div className="container">
+          <IYarlesNavbar />
           <Main />
         </div>
+
+
         <NextScript />
+        <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+        <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/collapse.js"></script>
       </body>
     </Html>
   )
---
+++ pages/_navbar.tsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import {
+    Navbar,
+    MobileNav,
+    Typography,
+    Button,
+    IconButton,
+} from "@material-tailwind/react";
+
+// Navbar borrowed from https://www.material-tailwind.com/docs/react/navbar
+export default function IYarlesNavbar() {
+    const navItems = [
+        { name: "About", href: "/" },
+        { name: "Resume", href: "/resume" },
+        { name: "Github", href: "https://github.com/Yarles404" }
+    ];
+
+    const [openNav, setOpenNav] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener(
+          "resize",
+          () => window.innerWidth >= 960 && setOpenNav(false)
+        );
+      }, []);
+
+    function navItem(name: string, href: string) {
+        return (
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+                key={name}
+            >
+                <a href={href} className="flex items-center">
+                    {name}
+                </a>
+            </Typography>
+        );
+    }
+
+    const navList = (
+        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            {navItems.map((item) => navItem(item.name, item.href))}
+        </ul>
+    );
+
+    return (
+        <Navbar className="py-2 lg:py-4">
+            <div className="container flex items-center justify-between text-blue-gray-900">
+                <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    className="mr-4 cursor-pointer py-1.5 font-normal"
+                >
+                    <span>IYarles</span>
+                </Typography>
+                <div className="hidden lg:block">{navList}</div>
+                <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+                    <span>Contact</span>
+                </Button>
+                <IconButton
+                    variant="text"
+                    className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                    ripple={false}
+                    onClick={() => setOpenNav(!openNav)}
+                >
+                    {openNav ? (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            className="h-6 w-6"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            />
+                        </svg>
+                    )}
+                </IconButton>
+            </div>
+            <MobileNav open={openNav}>
+                {navList}
+                <Button variant="gradient" size="sm" fullWidth className="mb-2">
+                    <span>Buy Now</span>
+                </Button>
+            </MobileNav>
+        </Navbar>
+    );
+}

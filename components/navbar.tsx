5f4430e48b9610903d4311930693c03998a89f56
Charles Yang <yarlescy@gmail.com>
2022-12-24 19:50:56 -0600

    adjusted navbar sizing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -48,67 +48,69 @@ export default function IYarlesNavbar() {
 
 
     return (
-        <Navbar className="mx-auto max-w-screen-xl py-2 lg:py-4">
-            <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-                <Typography
-                    as="a"
-                    href="#"
-                    variant="small"
-                    className="mr-4 cursor-pointer py-1.5 font-normal"
-                >
-                    <span>IYarles</span>
-                </Typography>
-                <div className="hidden lg:block">
-                    {navList}
+        <div className="container">
+            <Navbar className="mx-auto">
+                <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+                    <Typography
+                        as="a"
+                        href="#"
+                        variant="small"
+                        className="mr-4 cursor-pointer py-1.5 font-normal"
+                    >
+                        <span>IYarles</span>
+                    </Typography>
+                    <div className="hidden lg:block">
+                        {navList}
+                    </div>
+                    <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+                        <span>Contact</span>
+                    </Button>
+                    <IconButton
+                        title="Menu"
+                        variant="text"
+                        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                        ripple={false}
+                        onClick={() => setOpenNav(!openNav)}
+                    >
+                        {openNav ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                className="h-6 w-6"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            </svg>
+                        )}
+                    </IconButton>
                 </div>
-                <Button variant="gradient" size="sm" className="hidden lg:inline-block">
-                    <span>Contact</span>
-                </Button>
-                <IconButton
-                    title="Menu"
-                    variant="text"
-                    className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                    ripple={false}
-                    onClick={() => setOpenNav(!openNav)}
-                >
-                    {openNav ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            className="h-6 w-6"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                    ) : (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M4 6h16M4 12h16M4 18h16"
-                            />
-                        </svg>
-                    )}
-                </IconButton>
-            </div>
-            <MobileNav open={openNav}>
-                {navList}
-                <Button variant="gradient" size="sm" fullWidth className="mb-2">
-                    <span>Contact</span>
-                </Button>
-            </MobileNav>
-        </Navbar>
+                <MobileNav open={openNav}>
+                    {navList}
+                    <Button variant="gradient" size="sm" fullWidth className="mb-2">
+                        <span>Contact</span>
+                    </Button>
+                </MobileNav>
+            </Navbar>
+        </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 
 function HomePage() {
   return (
-    <div className='card'>
-      <div className='columns-2'>
-        <div className='h-full'>
-          <h1 className='text-6xl font-bold object-left'>Charles Yang</h1>
+    <div className="container">
+      <div className='card'>
+        <div className='columns-2'>
+          <div className='h-full'>
+            <h1 className='text-6xl font-bold object-left'>Charles Yang</h1>
+          </div>
+          <img src='/images/filler.png' alt='filler image' className='w-full' />
         </div>
-        <img src='/images/filler.png' alt='filler image' className='w-full' />
       </div>
     </div>
   )

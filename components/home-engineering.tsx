a9bdf5eba8fedeee4ec933be5a4daafbc6062e02
Charles Yang <yarlescy@gmail.com>
2022-12-26 15:07:59 -0600

    adjsuted spacing for small screens

--- components/home-engineering.tsx
+++ components/home-engineering.tsx
@@ -2,11 +2,11 @@ import { Card, CardBody } from '@material-tailwind/react'
 
 export default function HomeEngineering() {
     return (
-        <Card className='h-full relative bg-[url(../public/images/code-sample.jpg)] bg-contain bg-center bg-no-repeat my-2'>
+        <Card className='h-full relative bg-[url(../public/images/code-sample.jpg)] bg-cover bg-center bg-no-repeat my-2'>
             <div
                 className='absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-l sm:from-white/95 sm:to-white/25'
             ></div>
-            <CardBody className='relative lg:h-screen'>
+            <CardBody className='relative py-32 lg:h-screen'>
                 <div className='flex flex-row-reverse items-center w-full h-full'>
                     <h1 className='text-3xl font-extrabold sm:text-5xl text-deep-purple-700'>
                         Software

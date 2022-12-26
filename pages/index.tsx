601e11ceea872bb0763baecb7f9b1bab7b3e2236
Charles Yang <yarlescy@gmail.com>
2022-12-26 14:57:07 -0600

    added engineering section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import HomeHero from '../components/home-hero'
 import HomeArchitecture from '../components/home-architecture'
+import HomeEngineering from '../components/home-engineering'
 
 function Home() {
   return (
     <>
       <HomeHero />
       <HomeArchitecture />
+      <HomeEngineering />
     </>
   )
 }

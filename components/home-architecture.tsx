148846c71dd0fd2223908b3c2f1b08ca4a8b7501
Charles Yang <yarlescy@gmail.com>
2022-12-27 16:09:19 -0600

    added subtext to home sections

--- components/home-architecture.tsx
+++ components/home-architecture.tsx
@@ -4,16 +4,20 @@ export default function HomeArchitecture() {
     return (
         <Card className='h-full relative bg-[url(../public/images/iyarles2-architecture.png)] bg-cover bg-center bg-no-repeat my-2'>
             <div
-                className='absolute inset-0 bg-white/25 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/10'
+                className='absolute inset-0 bg-white/50 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/10'
             ></div>
-            <CardBody className='relative py-32 lg:h-screen'>
-                <div className='flex flex-row items-center w-full h-full'>
+            <CardBody className='relative py-32 items-center lg:flex lg:h-screen lg:items-center lg:px-8'>
+                {/* <div className='flex flex-row items-center w-full h-full'> */}
+                <div className='text-center sm:text-left'>
                     <h1 className='text-3xl font-extrabold sm:text-5xl text-deep-purple-700'>
                         Cloud
                         <strong className='block font-extrabold text-black'>
                             Architecture
                         </strong>
                     </h1>
+                    <p className='mt-4 bg-white rounded-xl max-w-lg sm:text-xl sm:leading-relaxed'>
+                        Analyzing requirements and creating cost-effective, scalable, and secure cloud solutions.
+                    </p>
                 </div>
             </CardBody>
         </Card>
--- components/home-engineering.tsx
+++ components/home-engineering.tsx
@@ -6,14 +6,17 @@ export default function HomeEngineering() {
             <div
                 className='absolute inset-0 bg-white/10 sm:bg-transparent sm:bg-gradient-to-l sm:from-white/95 sm:to-white/10'
             ></div>
-            <CardBody className='relative py-32 lg:h-screen'>
-                <div className='flex flex-row-reverse items-center w-full h-full'>
+            <CardBody className='relative py-32 lg:flex lg:h-screen lg:items-center lg:justify-end lg:px-8'>
+                <div className='text-center sm:text-right'>
                     <h1 className='text-3xl font-extrabold sm:text-5xl text-deep-purple-700'>
                         Software
                         <strong className='block font-extrabold text-black'>
                             Engineering
                         </strong>
                     </h1>
+                    <p className='mt-4 bg-white rounded-xl max-w-lg sm:text-xl sm:leading-relaxed'>
+                        Writing clean and maintainable code that gets the job done. Limit overhead and increase productivity.
+                    </p>
                 </div>
             </CardBody>
         </Card>

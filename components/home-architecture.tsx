8f3cf54663dd51406979cd6e3a84d6367cd5ad04
Charles Yang <yarlescy@gmail.com>
2022-12-26 21:48:31 -0600

    adjusted white bg opacity

--- components/home-architecture.tsx
+++ components/home-architecture.tsx
@@ -4,7 +4,7 @@ export default function HomeArchitecture() {
     return (
         <Card className='h-full relative bg-[url(../public/images/iyarles2-architecture.png)] bg-cover bg-center bg-no-repeat my-2'>
             <div
-                className='absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25'
+                className='absolute inset-0 bg-white/25 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/10'
             ></div>
             <CardBody className='relative py-32 lg:h-screen'>
                 <div className='flex flex-row items-center w-full h-full'>
--- components/home-engineering.tsx
+++ components/home-engineering.tsx
@@ -4,7 +4,7 @@ export default function HomeEngineering() {
     return (
         <Card className='h-full relative bg-[url(../public/images/code-sample.jpg)] bg-cover bg-center bg-no-repeat my-2'>
             <div
-                className='absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-l sm:from-white/95 sm:to-white/25'
+                className='absolute inset-0 bg-white/10 sm:bg-transparent sm:bg-gradient-to-l sm:from-white/95 sm:to-white/10'
             ></div>
             <CardBody className='relative py-32 lg:h-screen'>
                 <div className='flex flex-row-reverse items-center w-full h-full'>

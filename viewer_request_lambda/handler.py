74017f7ec36a92447a40d5f3925873f51bf938a5
Charles Yang <yarlescy@gmail.com>
2022-12-28 13:57:43 -0600

    fixed viewer request lambda [infra]
    prevent lambda from appending .html to root requests to domain

--- viewer_request_lambda/handler.py
+++ viewer_request_lambda/handler.py
@@ -1,7 +1,8 @@
 def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
 
-    if '.' not in request['uri']:
+    missing_extension = '.' not in request['uri'] and request['uri'][-1] != '/'
+    if missing_extension:
         request['uri'] += '.html'
     
     return request

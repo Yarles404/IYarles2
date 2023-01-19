bace9bbe26a987624d45d5dbcded3b1d79a72939
Charles Yang <yarlescy@gmail.com>
2023-01-18 21:32:59 -0600

    started philosophy section

--- components/home-philosophy.tsx
+++ components/home-philosophy.tsx
@@ -1,5 +1,8 @@
 export default function HomePhilosophy() {
     return (
-        <h1>Ni Hao</h1>
+        <div className="container my-2 lg:h-screen lg:px-8">
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem consequuntur asperiores tenetur ducimus modi reiciendis doloremque, quaerat sint necessitatibus deleniti voluptatum corrupti, esse molestias ex fugiat nisi? Rem, impedit corrupti.</p>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import HomeHero from '../components/home-hero'
 import HomeArchitecture from '../components/home-architecture'
 import HomeEngineering from '../components/home-engineering'
 import HomeStack from '../components/home-stack'
+import HomePhilosophy from '../components/home-philosophy'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HomeHero />
       <HomeEngineering />
       <HomeArchitecture />
+      <HomePhilosophy />
       <HomeStack />
     </>
   )

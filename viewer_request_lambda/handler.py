e1ba909ed3aa448177a35e9fb81e81c6d65df002
Charles Yang <yarlescy@gmail.com>
2023-11-05 17:19:30 -0600

    cleanup [skip ci]

--- viewer_request_lambda/handler.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    request = event['Records'][0]['cf']['request']
-
-    missing_extension = '.' not in request['uri'] and request['uri'][-1] != '/'
-    if missing_extension:
-        request['uri'] += '.html'
-    
-    return request
